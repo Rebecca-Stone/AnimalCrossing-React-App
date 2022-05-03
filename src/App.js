76615bed1472523b4a81f23a3c0b81f9c381e4b9
Rebecca-Stone <primstone16@gmail.com>
2022-05-02 19:29:00 -0500

    able to pull up information needed for each animal

--- src/App.js
+++ src/App.js
@@ -4,34 +4,48 @@ import { VILLAGERS_URL } from "./constants/index";
 import "./App.css";
 
 function App() {
+  // this contains ALL of villagers information
   const [data, setData] = useState([]);
 
   const fetchVillagers = () => {
     axios
       .get(`${VILLAGERS_URL}`)
       .then((res) => {
-        console.log("data:", res.data);
+        // console.log("data:", res.data);
         setData(res.data);
       })
       .catch((err) => console.log(err));
   };
 
   const createVillagers = () => {
-    const villagers = [];
-
+    const allVillagers = [];
     for (let i = 0; i < data.length; i++) {
-      villagers.push(<div key={i}>{data[i].name["name-USen"]}</div>);
-      // console.log("villagers name:", data[i].name["name-USen"]);
+      allVillagers.push(
+        <div key={i}>
+          <img src={data[i].icon_uri} alt={data[i].name} />
+          <h2>
+            Animal Name: {data[i].name["name-USen"]} ({data[i].gender})
+          </h2>
+          <h3>Birthday: {data[i]["birthday-string"]}</h3>
+          <h3>Hobby: {data[i].hobby}</h3>
+          <h3>Personality: {data[i].personality}</h3>
+          <h3>Catchphrase: {data[i]["catch-phrase"]}</h3>
+          <h3>Saying: {data[i].saying}</h3>
+        </div>
+      );
     }
-    // console.log("villagers:", villagers);
-    return villagers;
+    return allVillagers;
   };
 
   useEffect(() => {
     fetchVillagers();
   }, []);
 
-  return <div className="App">{createVillagers()}</div>;
+  return (
+    <div className="App">
+      <div className="Animals">{createVillagers()}</div>
+    </div>
+  );
 }
 
 export default App;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,5 @@
 export const VILLAGERS_URL = "http://acnhapi.com/v1a/villagers/";
+
+// export const VILLAGERS_URL = "https://api.nookipedia.com/villagers";
+
+// export const API_KEY = "2b961811-e3f4-42db-8a16-09cfa905e81a";
