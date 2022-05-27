a67c8a1a6aff95b28b6dcb5110361317094742b5
Rebecca-Stone <primstone16@gmail.com>
2022-05-27 13:37:41 -0500

    more changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,1 @@
 export const VILLAGERS_URL = "https://acnhapi.com/v1a/villagers/";
-
-// export const VILLAGERS_URL = "https://api.nookipedia.com/villagers";
-
-// export const API_KEY = "2b961811-e3f4-42db-8a16-09cfa905e81a";
